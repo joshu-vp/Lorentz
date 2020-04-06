6a202cca17943917157bd2ca51be0b5922aedc5f
lyda03 <josuemv21@hotmail.com>
2020-04-06 01:42:03 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,25 +28,25 @@ function draw() {
   points.push(new p5.Vector(x, y, z));
 
   translate(0, 0, -80);
-  let camX = map(mouseX, 0, width, -200, 200);
-  let camY = map(mouseY, 0, height, -200, 200);
+  let camX = map(mouseX, 0, width, - 2 * width, 2 * width);
+  let camY = map(mouseY, 0, height, - 2 * width, 2 * width);
   camera(camX, camY, (height / 2.0) / tan(PI * 30.0 / 180.0), 0, 0, 0, 0, 1, 0);
   //translate(width/2, height/2);
   scale(5);
-  stroke(255);
+  //stroke(255);
   noFill();
 
   let hu = 0;
-  beginShape();
-
+strokeWeight(1)
+  beginShape();    
   for (let v of points) {
-    stroke(hu, 255, 255);
+    stroke(0, 255, 255);
     vertex(v.x, v.y, v.z);
     //PVector offset = PVector.random3D();
     //offset.mult(0.1);
     //v.add(offset);
 
-    hu += 1;
+    hu += 0.1;
     if (hu > 255) {
       hu = 0;
     }
